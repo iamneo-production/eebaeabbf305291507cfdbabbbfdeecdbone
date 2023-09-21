40788b024f470c31690997476753c7dc6766f8fe
Iamneo <techadmin@iamneo.ai>
2023-09-21 09:38:21 +0000

    Backup Commit

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,18 @@
 terraform {
     required_providers{
-        
+        aws = {
+            source = ""
+
+        }
     }
 }
+provider "aws" {
+    region = "Singapore"
+    access_key= "SUSE Linu"
+    secret_key= ""
+
+}
+resource "aws_instance" "linux_server"{
+    ami= " "
+    instance_type =
+    }
