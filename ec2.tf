5ebde6b51c79f0ec3bfe5982f982e6f84cfbac62
Iamneo <techadmin@iamneo.ai>
2023-09-21 09:43:20 +0000

    Backup Commit

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,21 @@
 terraform {
     required_providers{
         aws = {
-            source = ""
+            source = "hashicorp/aws"
 
         }
     }
 }
 provider "aws" {
     region = "Singapore"
-    access_key= "SUSE Linu"
+    access_key= ""
     secret_key= ""
 
 }
 resource "aws_instance" "linux_server"{
-    ami= " "
-    instance_type =
+    ami= "SUSE Linux Enterprise Server 12 SP5"
+    instance_type ="t2.micro"
     }
+output "public_ip"{
+    value=aws_instance.demo_instance.public_ip
+}
